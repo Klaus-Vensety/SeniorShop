c54e80515b66e607513cf2f4677de417d2431819
Klaus-Vensety <imanbay15@gmail.com>
2026-04-18 16:39:14 +0500

    небольшие изменения склада

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,24 @@ int main() {
     SetConsoleOutputCP(1251);
 #endif
 
+    std::cout << "===== Магазин телефонов =====" << std::endl;
+
     Storage warehouse;
 
-    // Добавляем товары
-    warehouse.addStorageItem(Product(1, "Ноутбук", 55000.0, 5));
-    warehouse.addStorageItem(Product(2, "Мышь", 1500.0, 20));
-    warehouse.addStorageItem(Product(3, "Клавиатура", 3500.0, 15));
+    // Добавляем телефоны через новый конструктор
+    warehouse.addStorageItem(Product(1, "Apple", "iPhone 15 Pro", 256, 129990.0, 5));
+    warehouse.addStorageItem(Product(2, "Samsung", "Galaxy S25", 512, 89990.0, 10));
+    warehouse.addStorageItem(Product(3, "Xiaomi", "14 Ultra", 512, 69990.0, 15));
+    warehouse.addStorageItem(Product(4, "Google", "Pixel 9 Pro", 128, 79990.0, 8));
 
     // Пытаемся добавить дубликат ID
-    warehouse.addStorageItem(Product(2, "Мышь беспроводная", 2000.0, 10));
+    warehouse.addStorageItem(Product(2, "Samsung", "Galaxy S25+", 256, 99990.0, 5));
 
     // Показываем склад
     warehouse.showStorage();
 
     // Меняем цену
-    warehouse.changePrice(1, 52000.0);
+    warehouse.changePrice(1, 127990.0);
 
     // Удаляем товар
     warehouse.removeStorageItem(3);
